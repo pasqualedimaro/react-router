56b2923b5c4e44809795b1edb75f2c45f7cf2c0c
pasqualedimaro <pakydm15@gmail.com>
2025-07-24 17:39:55 +0200

    add axios usestate and useeffect for product details

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,12 +1,28 @@
-//funzione
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function ProductDetail() {
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(`https://fakestoreapi.com/products/${id}`)
+      .then((res) => setProduct(res.data))
+      .catch((err) => console.error("Errore nel caricamento prodotto:", err));
+  }, [id]);
+
+  if (!product) return <p>Caricamento...</p>;
+
   return (
     <div>
-      <h3>Titolo del prodotto</h3>
-      <p>Descrizione del prodotto</p>
+      <h2>{product.title}</h2>
+      <img src={product.image} alt={product.title} width="150" />
+      <p>{product.description}</p>
+      <p><strong>Prezzo:</strong> ${product.price}</p>
     </div>
   );
 }
 
-//esporto
 export default ProductDetail;
