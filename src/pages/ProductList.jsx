a1d3efb801d03fcecadaf9e7c56679e6e4dd1d05
pasqualedimaro <pakydm15@gmail.com>
2025-07-24 17:19:00 +0200

    add image,price and description

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -14,18 +14,23 @@ function ProductList(){
         axios.get(endpoint).then((res) => setProducts(res.data)).catch((err) => console.error("errore",err))
     }, []);
 
-    return(
-        <div>
-            <h3>lista prodotti:</h3>
-            <ul>
-                {products.map((products)=>(
-                <li key={products.id}>
-                    <Link to={`/products/${products.id}`}>{products.title}</Link>
-                </li>
-                ))}
-            </ul>
-        </div>
-    )
+    return (
+    <div>
+      <h3>Lista prodotti:</h3>
+      <ul className="product-list">
+        {products.map((product) => (
+          <li key={product.id} className="product-item">
+            <h4>
+              <Link to={`/products/${product.id}`}>{product.title}</Link>
+            </h4>
+            <img src={product.image} alt={product.title} className="product-image" />
+            <p className="product-price"><strong>Prezzo:</strong> ${product.price}</p>
+            <p className="product-description">{product.description.substring(0, 100)}...</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 
 
 }
