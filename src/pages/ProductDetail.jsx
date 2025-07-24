62546e5f79b7ed69266f9573622d09cb727692d9
pasqualedimaro <pakydm15@gmail.com>
2025-07-24 17:30:56 +0200

    create page for product detail

---
+++ src/pages/ProductDetail.jsx
@@ -0,0 +1,12 @@
+//funzione
+function ProductDetail() {
+  return (
+    <div>
+      <h3>Titolo del prodotto</h3>
+      <p>Descrizione del prodotto</p>
+    </div>
+  );
+}
+
+//esporto
+export default ProductDetail;
