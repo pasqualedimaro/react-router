44b0b23c2b71198f379425c478e302be6d90f3da
pasqualedimaro <pakydm15@gmail.com>
2025-07-24 17:11:56 +0200

    add axios usestate and useeffect

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,6 +1,33 @@
+//importo 
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+
 //funzione
 function ProductList(){
-    return <h3>Lista Prodotti:</h3>
+    const [products, setProducts] = useState([])
+    //chiamata axios
+    const endpoint = "https://fakestoreapi.com/products";
+    useEffect(() => {
+        axios.get(endpoint).then((res) => setProducts(res.data)).catch((err) => console.error("errore",err))
+    }, []);
+
+    return(
+        <div>
+            <h3>lista prodotti:</h3>
+            <ul>
+                {products.map((products)=>(
+                <li key={products.id}>
+                    <Link to={`/products/${products.id}`}>{products.title}</Link>
+                </li>
+                ))}
+            </ul>
+        </div>
+    )
+
+
 }
 
 //esporto
